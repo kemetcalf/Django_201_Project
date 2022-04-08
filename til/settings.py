41584b87d637b26aa5c842382bf785299e4c681d
Kat <kemetcalf3.14@gmail.com>
2022-04-08 13:46:18 -0700

    Fixing Environment Variables

--- til/settings.py
+++ til/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+
 import environ
 from pathlib import Path
 
@@ -24,7 +25,7 @@ env = environ.Env()
 environ.Env.read_env()
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env(SECRET_KEY)
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
