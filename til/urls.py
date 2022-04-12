0a90fa83e09fed7ae759382c5f4ba6549ebfd629
Kat <kemetcalf3.14@gmail.com>
2022-04-12 11:31:38 -0700

    Profile detail view card, new post modal, static collected

--- til/urls.py
+++ til/urls.py
@@ -20,10 +20,12 @@ from django.urls import re_path
 from django.conf import settings
 from django.urls import path
 from feed import urls as feed_urls
+from profiles import urls as profiles_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include(feed_urls, namespace="feed")),
+    path("profile/", include(profiles_urls, namespace="profiles")),
     re_path("", include("allauth.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # Do not use Django to serve media in production(insecure); local dev only
